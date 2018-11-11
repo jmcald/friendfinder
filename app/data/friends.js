475a6a3102013990ff660b5998e73f95cdc7e13c
John Caldwell <jmcald0@gmail.com>
2018-11-10 20:52:21 -0600

    ugggh ok for now

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 const friends = [
 	{
 		name: "John",
-		photo: "friendfinder\app\public\images\John.jpg",
+		photo: "John.jpg",
 		scores: [4, 1, 5, 5, 2, 2, 3, 1, 5, 1]
 	},
 	{
@@ -14,12 +14,12 @@ const friends = [
 		name: "Hodge",
         photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Felis_silvestris_catus_lying_on_rice_straw.jpg/1280px-Felis_silvestris_catus_lying_on_rice_straw.jpg",
         // By Basile Morin - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=68508072
-		scores: [5, 1, 1, 1, 5, 5, 1, 2, 3, 4]
+		scores: [5, 1, 1, 5, 5, 5, 1, 2, 3, 4]
 	},
 	{
 		name: "Royal McPoyle",
 		photo: "http://birdyinfo.com/show/birdimage/19815/bird.jpg",
-		scores: [5, 5, 5, 5, 1, 5, 5, 5, 5, 5]
+		scores: [5, 5, 1, 1, 1, 5, 5, 5, 5, 5]
 	},
 ];
 
